c11dfadfce57be262c64f2444f6899812fac1fca
Mariem KAMMOUN <mariemkammoun01@gmail.com>
2025-08-08 12:26:54 +0100

    react+tailwind setup

---
+++ int_proj_front/src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App
